69133597fc53f03e24429a9e714ce3190a8ec266
OpenOI-Dev-team <144697157+xyt2013@users.noreply.github.com>
2024-12-10 13:54:09 +0800

    Update Sweaping_Mine.cpp

--- Sweaping_Mine.cpp
+++ Sweaping_Mine.cpp
@@ -90,20 +90,6 @@ void music()
 
 
 
-void showmap(int mine[Row][Col])//没问题
-{
-    //打印棋盘
-    for (int a = 0; a < Row; a++)
-    {
-        for (int b = 0; b < Col; b++)
-        {
-            printf("%d ", mine[a][b]);
-        }
-        printf("\n");
-    }
-}
-
-
 
 void Showmine(int mine[Row][Col])
 {
@@ -203,9 +189,6 @@ void Mouseoperation(int mine[Row][Col])
                 mine[c][r] -= 20;
                 OpenNULL(mine, c, r);
                 Findmine(mine, c, r);
-                //打印雷区
-                showmap(mine);
-
             }
         }
         else if (msg.message == WM_RBUTTONDOWN)//按右键插旗
